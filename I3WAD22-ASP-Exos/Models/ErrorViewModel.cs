6382ae582531060db1b79b20763e56cc52dedb35
Samuel Legrain <samuel.legrain@bstorm.be>
2022-12-07 16:31:07 +0100

    Ajoutez des fichiers projet.

---
+++ I3WAD22-ASP-Exos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace I3WAD22_ASP_Exos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
